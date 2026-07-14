7e20d4417747c3c3ce45961d05067202e0b0abe1
Tehnaaz <24eg106c63@anurag.edu.in>
2026-07-14 15:11:46 +0530

    fit model

--- week1.py
+++ week1.py
@@ -11,12 +11,22 @@ print(y)
 # Build the Architechture
 model = keras.Sequential()
 model.add(layers.Input(shape = (2,) )) #evry neuron recives 2 inputs
-model.add(layers.Dense(1 , activation = 'sigmoid' ))                                                                                                           
+model.add(layers.Dense(1 , activation = 'sigmoid' ))  
+#model.add(layers.Dense(4,))                                                                                                         
 model.summary()
 
 #compile
 model.compile( loss='mse' ,  
-    optimizer = keras.optimizers.SGD(learning_rate = 0.1))
+    optimizer = keras.optimizers.SGD(learning_rate = 0.1),
+    metrics = ['accuracy'])
+
+#train
+model.fit(x,
+          y,
+          epochs = 300)
+
+loss = model.evaluate( x , y )
+print(loss)
 
 
 
